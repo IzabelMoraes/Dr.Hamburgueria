9f8048c315c8f6286d7c698073e6f65fcf642721
Izabel Moraes <izabel.nascimento@maristavirtual.org.br>
2023-02-06 16:25:31 -0300

    Arrumando carrinho de compras

--- assets/js/models.js
+++ assets/js/models.js
@@ -1,8 +1,8 @@
 let modelsJson = [
-    {id:1, name:'Hambúrguer doce', img:'img/hambuguer-doce.jpg', price:[25.00, 29.00, 30.00], sizes:['Normal', 'Cheddar cremoso', 'Carne dupla'], description:''},
-    {id:2, name:'Hambúrguer com bacon', img:'img/hamburguer-bacon.jpg', price:[25.00, 29.00, 30.00], sizes:['Normal', 'Cheddar cremoso', 'Carne dupla'], description:''},
-    {id:3, name:'Hambúrguer de cheddar cremoso', img:'img/hamburguer-cheedar.png', price:[25.00, 29.00, 30.50], sizes:['Normal', 'Cheddar cremoso', 'Carne dupla'], description:''},
-    {id:4, name:'Hambúrguer de doritos', img:'img/hamburguer-doritos.jpg', price:[25.00, 29.00, 30.00], sizes:['Normal', 'Cheddar cremoso', 'Carne dupla'], description:''},
-    {id:5, name:'Hambúrguer vegano', img:'img/hamburguer-vegano.jpg', price:[25.00, 29.00, 30.00], sizes:['Normal', 'Cheddar cremoso', 'Carne dupla'], description:''},
-    {id:6, name:'Hambúrguer de frango', img:'img/hambúrguer-frango.jpg', price:[25.00, 29.00, 30.00], sizes:['Normal', 'Cheddar cremoso', 'Carne dupla'], description:''}
+    {id:1, name:'Hambúrguer doce', img:'http://127.0.0.1:5501/assets/img/hambuguer-doce.jpg', price:[25.00, 29.00, 30.00], sizes:['Normal', 'Cheddar cremoso', 'Carne dupla'], description:''},
+    {id:2, name:'Hambúrguer com bacon', img:'http://127.0.0.1:5501/assets/img/hamburguer-bacon.jpg', price:[25.00, 29.00, 30.00], sizes:['Normal', 'Cheddar cremoso', 'Carne dupla'], description:''},
+    {id:3, name:'Hambúrguer de cheddar cremoso', img:'http://127.0.0.1:5501/assets/img/hamburguer-cheedar.png', price:[25.00, 29.00, 30.50], sizes:['Normal', 'Cheddar cremoso', 'Carne dupla'], description:''},
+    {id:4, name:'Hambúrguer de doritos', img:'http://127.0.0.1:5501/assets/img/hamburguer-doritos.jpg', price:[25.00, 29.00, 30.00], sizes:['Normal', 'Cheddar cremoso', 'Carne dupla'], description:''},
+    {id:5, name:'Hambúrguer vegano', img:'http://127.0.0.1:5501/assets/img/hamburguer-vegano.jpg', price:[25.00, 29.00, 30.00], sizes:['Normal', 'Cheddar cremoso', 'Carne dupla'], description:''},
+    {id:6, name:'Hambúrguer de frango', img:'http://127.0.0.1:5501/assets/img/hambúrguer-frango.jpg', price:[25.00, 29.00, 30.00], sizes:['Normal', 'Cheddar cremoso', 'Carne dupla'], description:''}
 ];
